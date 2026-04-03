c33718916d72a3cc9358ceea4625fe6ca9dc607f
jingxk0430-hue <jingxk0430@gmail.com>
2026-04-03 18:59:23 +0800

    Delete project-slwl-admin-vue directory

--- project-slwl-admin-vue/src/utils/rem.js
+++
@@ -1,6 +0,0 @@
-export function resizeRem() {
-  const docEl = document.documentElement
-  const clientWidth = docEl.clientWidth
-  if (!clientWidth) return
-  document.getElementById('screen-container').style.fontSize = 100 * (clientWidth / 1920) + 'px'
-}
